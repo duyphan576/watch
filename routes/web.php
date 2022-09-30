7546857ed3fcd0eca53e367cb9e435ea0207217c
duyphan576 <74853096+duyphan576@users.noreply.github.com>
2022-09-30 23:19:42 +0700

    main template

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/index', function () {
+    return view('main/index');
+});
+
+Route::get('/cart', function () {
+    return view('main/cart');
+});
+
+Route::get('/categories', function () {
+    return view('main/categories');
+});
+
+Route::get('/checkout', function () {
+    return view('main/checkout');
+});
+
+Route::get('/contact', function () {
+    return view('main/contact');
+});
+
+Route::get('/product', function () {
+    return view('main/product');
 });
