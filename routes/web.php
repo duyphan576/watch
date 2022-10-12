b60903b8e219a6aaa3b1726a43eb9207b3d8a0d7
ZoloPro <73628269+ZoloPro@users.noreply.github.com>
2022-10-13 01:07:51 +0700

    Before change template

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
 
 Route::get('/', function () {
-    return view('main/index');
+    return view('welcome');
 });
 
 Route::get('/index', function () {
@@ -89,3 +89,6 @@ Route::get('/admin/widget', function () {
     return view('admin/widget');
 });
 
+Route::get('/register', function() {
+    return view('main/register');
+});
